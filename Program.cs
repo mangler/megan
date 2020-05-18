a92a801c99114b6f206f52175d03e1379ae38019
Danny Sit <dw_@outlook.com>
2020-05-18 10:58:33 -0500

    enhance poc to have better UI and add exclusion filter

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Threading;
+using System.Drawing;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 
 namespace search_files
 {
@@ -16,20 +20,49 @@ namespace search_files
                 extentions = GetExtensions();
                 if (extentions.Any())
                 {
-                    Console.WriteLine($"Searching {path} for {string.Join(',', extentions)}");
+                    var excludes = GetExclusions();
+                    Console.Clear();
+                    Console.WriteLine($"Searching path: {path}");
+                    Console.WriteLine($"For extensions: {string.Join(' ', extentions)}");
+                    Console.WriteLine($"Excluding files containing the name: {string.Join(' ', excludes)}{Environment.NewLine}");
                     try
                     {
+                        var results = new Dictionary<string, List<string>>
+                        {
+                            {Result.Ignored, new List<string>()},
+                            {Result.Found, new List<string>()},
+                        };
+
                         foreach (var file in Directory.GetFiles(path, "*.*", SearchOption.AllDirectories).Where(f => extentions.IndexOf(Path.GetExtension(f)) >= 0))
                         {
-                            Console.WriteLine(file);
+                            Thread.Sleep(500);
+                            results[excludes.Any(c => Path.GetFileName(file).Contains(c)) ? Result.Ignored : Result.Found].Add(file);
                         }
-                        Console.WriteLine("Done! Press any key to exit...");
+
+                        foreach (var kvp in results.Where(c => c.Value.Any()))
+                        {
+                            Console.WriteLine($"Files {kvp.Key}");
+                            foreach (var file in kvp.Value)
+                            {
+                                try
+                                {
+                                    using (var image = Image.Load(file))
+                                    {
+                                        Console.WriteLine($"\t{Path.GetFileName(file)} [{image.Width}x{image.Height}]");
+                                    }
+                                }
+                                catch (SixLabors.ImageSharp.UnknownImageFormatException)
+                                {
+                                    Console.WriteLine($"\t{Path.GetFileName(file)}");
+                                }
+                            }
+                            Console.WriteLine();
+                        }
                     }
                     catch (Exception ex)
                     {
                         Console.WriteLine($"Could not search: {ex.Message}");
                     }
-                    Console.ReadLine();
                 }
                 else
                 {
@@ -40,6 +73,43 @@ namespace search_files
             {
                 Console.WriteLine("Could not get a valid path");
             }
+            Console.WriteLine();
+            Console.WriteLine("Done! Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static List<string> GetExclusions()
+        {
+            try
+            {
+                var exclusions = new List<string>();
+                var stop = false;
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter x when done");
+                    Console.Write($"Enter exclusion pattern >> {exclusions.Count} <<: ");
+                    var candidate = Console.ReadLine();
+                    switch (candidate.ToLower())
+                    {
+                        case "x":
+                            stop = true;
+                            break;
+                        default:
+                            {
+                                exclusions.Add(candidate);
+                                break;
+                            }
+                    }
+                } while (!stop);
+
+                return exclusions;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return new List<string>();
+            }
         }
 
         static List<string> GetExtensions()
@@ -48,9 +118,10 @@ namespace search_files
             {
                 var extentions = new List<string>();
                 var stop = false;
-                Console.WriteLine("Enter x when done");
                 do
                 {
+                    Console.Clear();
+                    Console.WriteLine("Enter x when done");
                     Console.Write($"Enter an extension to search [i.e. .txt, .jpg] >> {extentions.Count} <<: ");
                     var candidate = Console.ReadLine();
                     switch (candidate.ToLower())
@@ -86,6 +157,7 @@ namespace search_files
                 var stop = false;
                 do
                 {
+                    Console.Clear();
                     Console.Write("Enter a valid path: ");
                     path = Console.ReadLine();
                     if (Directory.Exists(path))
@@ -103,4 +175,10 @@ namespace search_files
             }
         }
     }
+    internal class Result
+    {
+        public const string Found = "Found";
+        public const string Ignored = "Ignored";
+    }
+
 }
